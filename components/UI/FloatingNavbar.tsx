2916c301fe5389b382d082007eaf094a485fe94f
MORPHS01 <ayonimofe2202@gmail.com>
2024-10-21 21:56:03 +0100

    bug fixes

--- components/UI/FloatingNavbar.tsx
+++ components/UI/FloatingNavbar.tsx
@@ -57,7 +57,7 @@ function FloatingNavbar({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit max-[800px]:w-[80%]  fixed top-10 inset-x-0 mx-auto border border-white/[0.2] bg-black-100 rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5  items-center justify-center space-x-5",
+          "flex max-w-fit max-[800px]:w-[80%]  fixed top-10 inset-x-0 mx-auto border border-white/[0.2] bg-black-100 rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5 max-[800px]:px-5 max-[800px]:py-3  items-center justify-center space-x-5 max-[800px]:space-x-3",
           className
         )}
       >
@@ -66,11 +66,11 @@ function FloatingNavbar({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative items-center flex space-x-1 text-white hover:text-cyan"
+              "relative items-center flex text-white hover:text-cyan"
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="text-md max-[800px]:text-base">{navItem.name}</span>
+            <span className="text-md max-[800px]:text-sm">{navItem.name}</span>
           </Link>
         ))}
       </motion.div>
