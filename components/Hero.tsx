93ca0f6dc46d33f418b4f348dd7a3b93aa23e6a7
MORPHS01 <ayonimofe2202@gmail.com>
2024-10-21 21:32:15 +0100

    bug fixes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ function Hero() {
     <div className="pb-20 pt-36 h-screen">
       <div>
         <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
-        <Spotlight className="h-[80vh] w-[50vw] top-10 left-full overflow-hidden" fill="purple" />
+        <Spotlight className="h-[80vh] w-[50vw] top-10 left-full overflow-hidden max-[800px]:hidden" fill="purple" />
         <Spotlight className="left-80 top-28 h-[80vh] w-[100vw]" fill="blue" />
       </div>
 
