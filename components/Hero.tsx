2a89525dcdd8bd060562daeb5308bd2462f55713
MORPHS01 <ayonimofe2202@gmail.com>
2024-11-01 23:36:51 +0100

    bug fixes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 function Hero() {
 
   return (
-    <div className="pb-20 pt-36 h-screen max-sm:pt-10">
+    <div className="pb-20 pt-36 h-screen max-sm:pt-16">
       <div>
         <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
         <Spotlight className="h-[80vh] w-[50vw] top-10 left-full overflow-hidden max-[800px]:hidden" fill="purple" />
