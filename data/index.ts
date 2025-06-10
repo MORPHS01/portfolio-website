da8f93f9cd399c280b2e345a91e09f1e29171013
MORPHS01 <ayonimofe2202@gmail.com>
2025-06-10 01:31:18 +0100

    new project

--- data/index.ts
+++ data/index.ts
@@ -1,73 +1,69 @@
-
-
 export const navItems = [
-    { name: "About", link: "#about" },
-    { name: "Projects", link: "#projects" },
-    { name: "Experiences", link: "#experiences" },
-    { name: "Contact", link: "#contact" },
-  ];
-  
-  
-  export const projects = [
-    {
-      id: 1,
-      title: "Maris Beauty Salon",
-      des: "A business-focused website showcasing hair and beauty services, complete with an intuitive booking system for seamless customer appointments and scheduling.",
-      img: "/images/marisbeautysalon.png",
-      link: "https://marisbeautysalon.vercel.app/",
-    },
-    {
-      id: 2,
-      title: "Morphyy Dashboard Application",
-      des: "A modern, interactive dashboard with real-time data visualization and sleek UI design.",
-      img: "/images/morphyy-dashboard.png",
-      link: "https://morphyy-dashboard-application.vercel.app/dashboard",
-    },
-    {
-      id: 3,
-      title: "Bookmark Landing Page", 
-      des: "A clean, responsive landing page promoting a bookmarking tool with call-to-action features.",
-      img: "/images/bookmark.png",
-      link: "https://bookmarkcom.vercel.app/",
-    },
-    {
-      id: 4,
-      title: "Countries of the World",
-      des: "An app displaying detailed country data using APIs, including population, region, and capital info.",
-      img: "/images/countries-of-the-world.png",
-      link: "https://countries-of-the-world-chi.vercel.app/",
-    },
-    {
-      id: 5,
-      title: "Easybank Landing Page",
-      des: "A visually appealing landing page showcasing digital banking features with smooth animations and responsiveness.",
-      img: "/images/easybank.png",
-      link: "https://easybank-landing-page-master-ruby.vercel.app/",
-    },
-    {
-      id: 6,
-      title: "Age Calculator Application",
-      des: "A user-friendly tool that calculates precise age based on the birth date the user inputs.",
-      img: "/images/age-calculator.png",
-      link: "https://age-calculator-app-zeta-olive.vercel.app/",
-    },
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Experiences", link: "#experiences" },
+  { name: "Contact", link: "#contact" },
+];
 
-  ];
+export const projects = [
+  {
+    id: 1,
+    title: "ElectroBay E-commerce Website",
+    des: "A fully functional e-commerce website with product listings, shopping cart, and user authentication.",
+    img: "/images/electrobay.png",
+    link: "https://electrobay-store.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Maris Beauty Salon",
+    des: "A business-focused website showcasing hair and beauty services, complete with an intuitive booking system for seamless customer appointments and scheduling.",
+    img: "/images/marisbeautysalon.png",
+    link: "https://marisbeautysalon.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Morphyy Dashboard Application",
+    des: "A modern, interactive dashboard with real-time data visualization and sleek UI design.",
+    img: "/images/morphyy-dashboard.png",
+    link: "https://morphyy-dashboard-application.vercel.app/dashboard",
+  },
+  {
+    id: 4,
+    title: "Bookmark Landing Page",
+    des: "A clean, responsive landing page promoting a bookmarking tool with call-to-action features.",
+    img: "/images/bookmark.png",
+    link: "https://bookmarkcom.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "Countries of the World",
+    des: "An app displaying detailed country data using APIs, including population, region, and capital info.",
+    img: "/images/countries-of-the-world.png",
+    link: "https://countries-of-the-world-chi.vercel.app/",
+  },
+  {
+    id: 6,
+    title: "Age Calculator Application",
+    des: "A user-friendly tool that calculates precise age based on the birth date the user inputs.",
+    img: "/images/age-calculator.png",
+    link: "https://age-calculator-app-zeta-olive.vercel.app/",
+  },
+];
 
-  export const socialMedia = [
-    {
-      id: 1,
-      img: "/socials/whatsapp-svg.svg",
-      link:"https://wa.me/+2348025907227?text=Hi%2C%20I%20would%20like%20to%20discuss%20about%20your%20web%20development%20services",
-    },
-    {
-      id: 2,
-      img: "/socials/linkedin-svg.svg",
-      link:"https://www.linkedin.com/in/ayonimofe-atoyebi-4272b522b",
-    },
-    {
-      id: 3,
-      img: "/socials/github-svg.svg",
-      link:"https://github.com/MORPHS01",
-    },
-  ];
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/socials/whatsapp-svg.svg",
+    link: "https://wa.me/+2348025907227?text=Hi%2C%20I%20would%20like%20to%20discuss%20about%20your%20web%20development%20services",
+  },
+  {
+    id: 2,
+    img: "/socials/linkedin-svg.svg",
+    link: "https://www.linkedin.com/in/ayonimofe-atoyebi-4272b522b",
+  },
+  {
+    id: 3,
+    img: "/socials/github-svg.svg",
+    link: "https://github.com/MORPHS01",
+  },
+];
